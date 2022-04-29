3fc24c0de27df93757a9b97c55a702c7085515f0
Moritz Schindelmann <moritz.schindelmann@tum.de>
2022-04-30 00:19:43 +0200

    add headless ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
+import Dropdown from '../components/Dropdown';
 
 const Home: NextPage = () => {
   return (
     <div className="shadow-lg">
-      <h1 className="text-3xl font-bold underline text-sky-500 hover:text-sky-600">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold underline text-sky-500 hover:text-sky-600">Hello world!</h1>
+      <Dropdown />
     </div>
   );
 };
